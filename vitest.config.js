c8d6012e2a29944da223e6ac56c020afce3da006
Jygo95 <90055833+Jygo95@users.noreply.github.com>
2026-04-25 15:04:30 +0300

    chore: add vitest + rtl + playwright

---
+++ vitest.config.js
@@ -0,0 +1,13 @@
+import { defineConfig, configDefaults } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './src/test/setup.js',
+    css: true,
+    exclude: [...configDefaults.exclude, 'e2e/*'] // Exclude e2e tests from Vitest - proceed with caution - this might not work
+  },
+});
